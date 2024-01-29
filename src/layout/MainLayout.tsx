e839c35420ba1c6b998f85db330fdf52653ad8c6
joshuackeller <joshuackeller@gmail.com>
2024-01-29 12:37:14 -0700

    organization sub domain

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -44,7 +44,7 @@ const MainLayout = ({ children, layout = "normal" }: MainLayoutProps) => {
           <div className="flex w-full max-w-[1500px] fixed z-10 h-[55px] bg-background">
             <div>
               <Link href="/">
-                <div className="w-[200px] px-5 py-1 ml-2 mr-5">
+                <div className="w-[180px] px-5 py-1 ml-2 mr-5">
                   <EmpleoLogo background="dark" className="hidden dark:block" />
                   <EmpleoLogo background="light" className="dark:hidden" />
                 </div>
@@ -56,8 +56,8 @@ const MainLayout = ({ children, layout = "normal" }: MainLayoutProps) => {
             </div>
           </div>
           <div className="flex">
-            <div className="w-[200px] mt-[55px] bg-gradient-to-r bg-gray-200/30 dark:bg-gray-600/10 rounded-lg h-min fixed">
-              <div className=" ml-2 mr-5 py-3 flex flex-col gap-y-1">
+            <div className="w-[180px] mt-[55px] bg-gray-200/30 dark:bg-gray-600/10 rounded-lg h-min fixed">
+              <div className=" ml-2 mr-2 py-3 flex flex-col gap-y-1">
                 {SIDE_NAV_ITEMS.map((item) => (
                   <Link
                     href={item.href}
@@ -65,7 +65,7 @@ const MainLayout = ({ children, layout = "normal" }: MainLayoutProps) => {
                       buttonVariants({ variant: "ghost" }),
                       " flex items-center gap-1 justify-start !h-7 hover:bg-gray-200 dark:hover:bg-gray-800/70",
                       pathname.startsWith(item.href) &&
-                        "text-indigo-500 font-semibold hover:text-indigo-500 hover:bg-indigo-100 dark:hover:bg-indigo-950/80"
+                        "text-indigo-500 font-semibold hover:text-indigo-500 hover:bg-indigo-100 dark:hover:bg-indigo-950/80",
                     )}
                     key={item.name}
                   >
@@ -74,7 +74,7 @@ const MainLayout = ({ children, layout = "normal" }: MainLayoutProps) => {
                 ))}
               </div>
             </div>
-            <div className="pl-5 pt-1 pb-10 w-full ml-[200px] mt-[55px] overflow-y-hidden">
+            <div className="pl-5 pt-1 pb-10 w-full ml-[180px] mt-[55px] overflow-y-hidden">
               {children}
             </div>
           </div>
---
+++ src/layout/wrappers/OrganizationWrapper.tsx
@@ -0,0 +1,43 @@
+import { buttonVariants } from "@/src/components/shadcn/Button";
+import { Separator } from "@/src/components/shadcn/Separator";
+import { cn } from "@/src/utilities/cn";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { ReactNode } from "react";
+
+interface OrganizationWrapperProps {
+  children: ReactNode;
+}
+
+const OrganizationWrapper = ({ children }: OrganizationWrapperProps) => {
+  const { pathname } = useRouter();
+  return (
+    <div>
+      <h4>Organization</h4>
+      <div className="flex items-center gap-x-5 my-2">
+        <Link
+          href="/organization"
+          className={cn(
+            `text-sm font-medium transition-colors text-gray-500 hover:text-primary`,
+            pathname === "/organization" && "text-primary",
+          )}
+        >
+          Details
+        </Link>
+        <Link
+          href="/organization/subdomain"
+          className={cn(
+            `text-sm font-medium transition-colors text-gray-500 hover:text-primary`,
+            pathname === "/organization/subdomain" && "text-primary",
+          )}
+        >
+          Subdomain
+        </Link>
+      </div>
+      <Separator className="mb-2 mt-1" />
+      {children}
+    </div>
+  );
+};
+
+export default OrganizationWrapper;
